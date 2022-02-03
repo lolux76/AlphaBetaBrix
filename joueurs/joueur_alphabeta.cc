ccc9d9b2337d460eaeb11effbdd6118415059ee9
BuildTools <unconfigured@null.spigotmc.org>
2022-02-03 14:53:48 +0100

    Ajout d'une méthode 'rechercheCoupValide' dans la classe Joueur_AlphaBeta qui renvois un vector de tous les coups possibles pour ce tour.

--- joueurs/joueur_alphabeta.cc
+++ joueurs/joueur_alphabeta.cc
@@ -12,7 +12,67 @@ Joueur_AlphaBeta::Joueur_AlphaBeta(std::string nom, bool joueur)
     return 'A';
 }
 */
+
+std::vector<Brix> Joueur_AlphaBeta::rechercheCoupValide(Jeu jeu){
+    
+    //VARIABLES LOCALES
+    std::vector<Brix> coupValide;
+    Brix b_candidate;
+    int tour = jeu.nbCoupJoue()+1;//la b_candidate devra être valide au tour auquel on va la jouer,i.e. au tour suivant
+
+    //ALGO QUI LISTE LES COUPS POSSIBLES
+
+    for(int abscisse = 0; abscisse < MAX_LARGEUR; abscisse++){ // Pour chaque ligne on va chercher dans chaque colonne
+        int ordonnee = 0;
+
+        while(ordonnee < MAX_HAUTEUR && jeu.plateau()[ordonnee][abscisse] != '0'){ //Tant que la case n'est pas jouable et la colonne n'est pas pleine
+            ordonnee++; //On cherche à la case plus haute
+        }
+
+        if(ordonnee < MAX_HAUTEUR){ //On a une case jouable qui est dans les limites du jeu
+            //On cherche tous les coups valide à partir de cette case vide
+            b_candidate.setAllCoord(abscisse, ordonnee, abscisse, ordonnee + 1); //Brix verticale de bottom 'X'
+            if (jeu.coup_licite(b_candidate, tour))  //Le coup est valide
+            {
+                coupValide.push_back(b_candidate); //On ajoute notre coup à la liste des coups jouables
+            }
+
+            b_candidate.setAllCoord(abscisse,ordonnee+1,abscisse,ordonnee); //Brix verticale de bottom est 'O'
+            if (jeu.coup_licite(b_candidate,tour))
+            {
+                coupValide.push_back(b_candidate);
+            }
+
+            b_candidate.setAllCoord(abscisse,ordonnee ,abscisse+1,ordonnee); //Brix horizontale commençant par 'X'
+            if (jeu.coup_licite(b_candidate,tour))
+            {
+                coupValide.push_back(b_candidate);
+            }
+
+            b_candidate.setAllCoord(abscisse+1,ordonnee,abscisse,ordonnee); //Brix terminant commençant par 'X'
+            if (jeu.coup_licite(b_candidate,tour))
+            {
+                coupValide.push_back(b_candidate);
+            }
+        }
+        //Fin des Brix valide à cette abscisse et cette ordonnee, on passe à l'abscisse suivante
+    }
+    //Affichage de la liste des coups valides pour vérification
+        for(Brix b : coupValide){
+            std::cout << b << " | ";
+        }
+        std::cout << std::endl;
+        
+        
+        return coupValide;
+}
+
+void alphabeta(char noeud, int alpha, int beta){
+    //algorithme alpha beta de base
+
+}
+
 void Joueur_AlphaBeta::recherche_coup(Jeu jeu, Brix &coup)
 {
-
+    rechercheCoupValide(jeu);
 }
--- joueurs/joueur_alphabeta.hh
+++ joueurs/joueur_alphabeta.hh
@@ -2,7 +2,11 @@
 
 
 #include "joueur.hh"
+#include <vector>
+#include "../arbitre.hh"
 
+#define PLUS_INFINI = std::numeric_limits<int>::max(); //+ infini, NE JAMAIS FAIRE PLUS_INFINI + QQCHOSE (dépassement de bits)
+#define MOINS_INFINI = std::numeric_limits<int>::min(); //- infini, NE JAMAIS FAIRE MOINS_INFINI - QQCHOSE (dépassement de bits)
 
 class Joueur_AlphaBeta : public Joueur
 {
@@ -10,5 +14,10 @@ public:
   Joueur_AlphaBeta(std::string nom, bool joueur);
   //char nom_abbrege() const override;
 
+  std::vector<Brix> rechercheCoupValide(Jeu jeu);
+
+  void alphabeta(char noeud, int alpha, int beta); //Algorithme alpha beta de base
+
   void recherche_coup(Jeu jeu, Brix & coup) override;
+
 };
