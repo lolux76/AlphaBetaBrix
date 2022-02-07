6473d7250ff1fde2482894ba73a95c1c038087b8
BuildTools <unconfigured@null.spigotmc.org>
2022-02-07 16:51:38 +0100

    Implantation de l'algorithme Alpha Beta. Calcul du meilleur chemin

--- joueurs/joueur_alphabeta.cc
+++ joueurs/joueur_alphabeta.cc
@@ -2,10 +2,9 @@
 #include <memory>
 
 Joueur_AlphaBeta::Joueur_AlphaBeta(std::string nom, bool joueur)
-    :Joueur(nom,joueur)
-{}
-
-
+    : Joueur(nom, joueur)
+{
+}
 
 /*char Joueur_AlphaBeta::nom_abbrege() const
 {
@@ -13,66 +12,130 @@ Joueur_AlphaBeta::Joueur_AlphaBeta(std::string nom, bool joueur)
 }
 */
 
-std::unique_ptr<std::vector<Brix>> Joueur_AlphaBeta::rechercheCoupValide(Jeu jeu){
-    
-    //VARIABLES LOCALES
+std::unique_ptr<std::vector<Brix>> Joueur_AlphaBeta::rechercheCoupValide(Jeu jeu)
+{
+
+    // VARIABLES LOCALES
     std::unique_ptr<std::vector<Brix>> coupValide;
     Brix b_candidate;
-    int tour = jeu.nbCoupJoue()+1;//la b_candidate devra être valide au tour auquel on va la jouer,i.e. au tour suivant
+    int tour = jeu.nbCoupJoue() + 1; // la b_candidate devra être valide au tour auquel on va la jouer,i.e. au tour suivant
 
-    //ALGO QUI LISTE LES COUPS POSSIBLES
+    // ALGO QUI LISTE LES COUPS POSSIBLES
 
-    for(int abscisse = 0; abscisse < MAX_LARGEUR; abscisse++){ // Pour chaque ligne on va chercher dans chaque colonne
+    for (int abscisse = 0; abscisse < MAX_LARGEUR; abscisse++)
+    { // Pour chaque ligne on va chercher dans chaque colonne
         int ordonnee = 0;
 
-        while(ordonnee < MAX_HAUTEUR && jeu.plateau()[ordonnee][abscisse] != '0'){ //Tant que la case n'est pas jouable et la colonne n'est pas pleine
-            ordonnee++; //On cherche à la case plus haute
+        while (ordonnee < MAX_HAUTEUR && jeu.plateau()[ordonnee][abscisse] != '0')
+        {               // Tant que la case n'est pas jouable et la colonne n'est pas pleine
+            ordonnee++; // On cherche à la case plus haute
         }
 
-        if(ordonnee < MAX_HAUTEUR){ //On a une case jouable qui est dans les limites du jeu
-            //On cherche tous les coups valide à partir de cette case vide
-            b_candidate.setAllCoord(abscisse, ordonnee, abscisse, ordonnee + 1); //Brix verticale de bottom 'X'
-            if (jeu.coup_licite(b_candidate, tour))  //Le coup est valide
+        if (ordonnee < MAX_HAUTEUR)
+        { // On a une case jouable qui est dans les limites du jeu
+            // On cherche tous les coups valide à partir de cette case vide
+            b_candidate.setAllCoord(abscisse, ordonnee, abscisse, ordonnee + 1); // Brix verticale de bottom 'X'
+            if (jeu.coup_licite(b_candidate, tour))                              // Le coup est valide
             {
-                coupValide->push_back(b_candidate); //On ajoute notre coup à la liste des coups jouables
+                coupValide->push_back(b_candidate); // On ajoute notre coup à la liste des coups jouables
             }
 
-            b_candidate.setAllCoord(abscisse,ordonnee+1,abscisse,ordonnee); //Brix verticale de bottom est 'O'
-            if (jeu.coup_licite(b_candidate,tour))
-            {
-                coupValide->push_back(b_candidate);
-            }
-
-            b_candidate.setAllCoord(abscisse,ordonnee ,abscisse+1,ordonnee); //Brix horizontale commençant par 'X'
-            if (jeu.coup_licite(b_candidate,tour))
+            b_candidate.setAllCoord(abscisse, ordonnee + 1, abscisse, ordonnee); // Brix verticale de bottom est 'O'
+            if (jeu.coup_licite(b_candidate, tour))
             {
                 coupValide->push_back(b_candidate);
             }
 
-            b_candidate.setAllCoord(abscisse+1,ordonnee,abscisse,ordonnee); //Brix terminant commençant par 'X'
-            if (jeu.coup_licite(b_candidate,tour))
+            b_candidate.setAllCoord(abscisse, ordonnee, abscisse + 1, ordonnee); // Brix horizontale commençant par 'X'
+            if (jeu.coup_licite(b_candidate, tour))
+            {
+                coupValide->push_back(b_candidate);
+            }
+
+            b_candidate.setAllCoord(abscisse + 1, ordonnee, abscisse, ordonnee); // Brix terminant commençant par 'X'
+            if (jeu.coup_licite(b_candidate, tour))
             {
                 coupValide->push_back(b_candidate);
             }
         }
-        //Fin des Brix valide à cette abscisse et cette ordonnee, on passe à l'abscisse suivante
+        // Fin des Brix valide à cette abscisse et cette ordonnee, on passe à l'abscisse suivante
     }
-    //Affichage de la liste des coups valides pour vérification
-        for(auto b : *coupValide){
-            std::cout << b << " | ";
-        }
-        std::cout << std::endl;
-        
-        
-        return coupValide;
+    // Affichage de la liste des coups valides pour vérification
+    for (auto b : *coupValide)
+    {
+        std::cout << b << " | ";
+    }
+    std::cout << std::endl;
+
+    return coupValide;
 }
 
-void alphabeta(char noeud, int alpha, int beta){
-    //algorithme alpha beta de base
+int Joueur_AlphaBeta::alphabeta(Jeu &jeu, int &alpha, int &beta, std::chrono::high_resolution_clock::time_point &start, unsigned int &profondeur_max, unsigned int profondeur)
+{
+    // algorithme alpha beta de base
 
+    /***
+     * 
+     * TODO
+     * récupérer le coup à jouer
+     * idée : variable meilleur_coup
+     * 
+     * Ensuite tester
+     * 
+    ***/
+
+   //Vérification qu'il nous reste du temps pour jouer
+    if (std::chrono::high_resolution_clock::now() - start > std::chrono::milliseconds(TEMPS_POUR_UN_COUP - 100))
+    {
+        start -= std::chrono::milliseconds(50000);
+        return alphabeta(jeu, alpha, beta, start, profondeur, profondeur); //On évalue la feuille actuelle car manque de temps pour explorer
+    }
+
+    //On est sur une feuille
+    if (profondeur == profondeur_max)
+    {
+        return rand();
+    }
+
+    //On doit faire un appel récursif pour continuer
+    auto coups_valides = std::move(rechercheCoupValide(jeu)); // Liste des coups valides
+    bool NoeudMin = false; //Le noeud est-il un noeud min ou max?
+    if (profondeur % 2 == 0)
+        NoeudMin = true;
+
+    int score = 0; //Pour le moment aucun coups n'est joué, donc la valeur actuelle de la partie est nulle
+
+    //Pour chaque coup possible, on explore l'arbre
+    for (auto coups : *coups_valides)
+    {
+        Jeu jeuBis = jeu;
+        jeuBis.joue(coups);
+        score = alphabeta(jeuBis, alpha, beta, start, profondeur_max, profondeur + 1);
+        if (NoeudMin)
+        {
+            if (alpha > score)
+                alpha = score;
+            if (alpha <= beta)
+                return score;
+        }
+        else
+        {
+            if (beta < score)
+                beta = score;
+            if (beta >= score)
+                return score;
+        }
+    }
+    // TODO:
+    return score;
 }
 
 void Joueur_AlphaBeta::recherche_coup(Jeu jeu, Brix &coup)
 {
-    auto coups_valides = std::move(rechercheCoupValide(jeu));
+    auto start = std::chrono::high_resolution_clock::now();
+    unsigned int profondeur_max = 3;
+    unsigned int profondeur = 0;
+    int alpha = PLUS_INFINI;
+    int beta = MOINS_INFINI;
+    alphabeta(jeu, alpha, beta, start, profondeur_max, profondeur);
 }
--- joueurs/joueur_alphabeta.hh
+++ joueurs/joueur_alphabeta.hh
@@ -4,9 +4,10 @@
 #include <vector>
 #include "../arbitre.hh"
 #include <memory>
+#include <chrono>
 
-#define PLUS_INFINI = std::numeric_limits<int>::max();  //+ infini, NE JAMAIS FAIRE PLUS_INFINI + QQCHOSE (dépassement de bits)
-#define MOINS_INFINI = std::numeric_limits<int>::min(); //- infini, NE JAMAIS FAIRE MOINS_INFINI - QQCHOSE (dépassement de bits)
+#define PLUS_INFINI std::numeric_limits<int>::max()  //+ infini, NE JAMAIS FAIRE PLUS_INFINI + QQCHOSE (dépassement de bits)
+#define MOINS_INFINI std::numeric_limits<int>::min() //- infini, NE JAMAIS FAIRE MOINS_INFINI - QQCHOSE (dépassement de bits)
 
 class Joueur_AlphaBeta : public Joueur
 {
@@ -14,8 +15,8 @@ public:
   Joueur_AlphaBeta(std::string nom, bool joueur);
   //char nom_abbrege() const override;
 
-  std::unique_ptr<std::vector<Brix>> rechercheCoupValide(Jeu jeu);
-  void alphabeta(char noeud, int alpha, int beta); //Algorithme alpha beta de base
+  static std::unique_ptr<std::vector<Brix>> rechercheCoupValide(Jeu jeu);
+  int alphabeta(Jeu &jeu, int &alpha, int &beta, std::chrono::high_resolution_clock::time_point &start, unsigned int &profondeur_max, unsigned int profondeur); //Algorithme alpha beta de base
 
   void recherche_coup(Jeu jeu, Brix &coup) override;
 };
