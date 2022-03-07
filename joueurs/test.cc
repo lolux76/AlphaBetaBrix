b6eb09fb34f4335595d47235105681f0ebbd268c
Bertille Trotin <bertille.trotin@gmail.com>
2022-03-07 16:51:40 +0100

    correction info alignement diagonale

--- joueurs/test.cc
+++ joueurs/test.cc
@@ -12,39 +12,39 @@ int main()
     // Brix(int aX, int oX, int aO, int oO)
 
     // TEST VERTICAL :
-    Brix b1(3, 0, 2, 0);
-    Brix b2(3, 1, 2, 1);
-    Brix b3(3, 2, 2, 2);
-    Brix b4(3, 3, 2, 3);
+    // Brix b1(3, 0, 2, 0);
+    // Brix b2(3, 1, 2, 1);
+    // Brix b3(3, 2, 2, 2);
+    // Brix b4(3, 3, 2, 3);
 
-    jeux.joue(b1);
-    jeux.joue(b2);
-    jeux.joue(b3);
+    // jeux.joue(b1);
+    // jeux.joue(b2);
+    // jeux.joue(b3);
 
-    std::cout << jeux << std::endl;
+    // std::cout << jeux << std::endl;
 
-    info_coup info(b4, std::make_shared<Jeu>(jeux), 'x', 4);
+    // info_coup info(b4, std::make_shared<Jeu>(jeux), 'x', 4);
 
-    auto t1 = std::chrono::high_resolution_clock::now();
+    // auto t1 = std::chrono::high_resolution_clock::now();
 
-    info.alignement();
+    // info.alignement();
 
-    auto t2 = std::chrono::high_resolution_clock::now();
-    auto ms_int = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+    // auto t2 = std::chrono::high_resolution_clock::now();
+    // auto ms_int = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
 
-    std::cout << "alignement vertical : " << std::endl;
-    std::cout << info.accNbPieceAlignJ()[1] << std::endl;
-    std::cout << info.accNbPieceAlignA()[1] << std::endl;
+    // std::cout << "alignement vertical : " << std::endl;
+    // std::cout << info.accNbPieceAlignJ()[1] << std::endl;
+    // std::cout << info.accNbPieceAlignA()[1] << std::endl;
     
-        std::cout << "alignement horizontal : " << std::endl;
-    std::cout << info.accNbPieceAlignJ()[0] << std::endl;
-    std::cout << info.accNbPieceAlignA()[0] << std::endl;
+    //     std::cout << "alignement horizontal : " << std::endl;
+    // std::cout << info.accNbPieceAlignJ()[0] << std::endl;
+    // std::cout << info.accNbPieceAlignA()[0] << std::endl;
 
-    std::cout << "alignement diagonal : " << std::endl;
-    std::cout << info.accNbPieceAlignJ()[2] << std::endl;
-    std::cout << info.accNbPieceAlignA()[2] << std::endl;
+    // std::cout << "alignement diagonal : " << std::endl;
+    // std::cout << info.accNbPieceAlignJ()[2] << std::endl;
+    // std::cout << info.accNbPieceAlignA()[2] << std::endl;
 
-    std::cout << "Temps écoulé : " << ms_int.count() << "us" << std::endl;
+    // std::cout << "Temps écoulé : " << ms_int.count() << "us" << std::endl;
 
     //// TEST HORIZONTAL :
     // Brix b5 (0,0,0,1);
@@ -68,31 +68,32 @@ int main()
     // std::cout << info2.accNbPieceAlignA()[0] << std::endl;
 
     // // TEST DIAGONALE :
-    // Brix b10(1,0,0,0);
-    // Brix b11(0,1,1,1);
-    // Brix b12(0,3,1,3);
-    // Brix b13(5,0,6,0);
-    // Brix b14(4,1,5,1);
-    // Brix b15(4,3,5,3);
-    // Brix b16(5,4,6,4);
-    // Brix b17(3,2,2,2);
+    Brix b10(1,0,0,0);
+    Brix b11(0,1,1,1);
+    Brix b12(0,3,1,3);
+    Brix b13(5,0,6,0);
+    Brix b14(4,1,5,1);
+    Brix b15(4,3,5,3);
+    Brix b16(5,4,6,4);
+    Brix b17(3,2,2,2);
 
-    // jeux.joue(b10);
-    // jeux.joue(b11);
-    // jeux.joue(b12);
-    // jeux.joue(b13);
-    // jeux.joue(b14);
-    // jeux.joue(b15);
-    // jeux.joue(b16);
+    jeux.joue(b10);
+    jeux.joue(b11);
+    jeux.joue(b12);
+    jeux.joue(b13);
+    jeux.joue(b14);
+    jeux.joue(b15);
+    jeux.joue(b16);
+    jeux.joue(b17);
 
-    // std::cout << jeux << std::endl;
+    std::cout << jeux << std::endl;
 
-    // info_coup info3(b17,std::make_shared<Jeu>(jeux),'x',4);
+    info_coup info3(b17,std::make_shared<Jeu>(jeux),'x',4);
 
-    // info3.alignement();
-    // std::cout << "alignement diagonale : "<<std::endl;
-    // std::cout << info3.accNbPieceAlignJ()[2]<< std::endl;
-    // std::cout << info3.accNbPieceAlignA()[2] << std::endl;
+    info3.alignement();
+    std::cout << "alignement diagonale : "<<std::endl;
+    std::cout << info3.accNbPieceAlignJ()[2]<< std::endl;
+    std::cout << info3.accNbPieceAlignA()[2] << std::endl;
 
     // // TEST HORS PLATEAU :
     // Brix b18(7,1,6,1);
